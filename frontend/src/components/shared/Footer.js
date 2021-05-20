b38a8e95ce10adebf73bfdbd2476dc65c910f2ad
Mohd Bilal <r.mohammedbilal@gmail.com>
2021-05-20 19:11:58 +0530

    Update Footer.js

--- frontend/src/components/shared/Footer.js
+++ frontend/src/components/shared/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div style={{bottom:"5px",paddingTop:'50px'}}>
-      Devoloped with ❤ by{" "}
+      Developed with ❤ by{" "}
       <a tarhet="_blank" href="https://github.com/mdb571">
         @mdb571
       </a>{"  & "}
